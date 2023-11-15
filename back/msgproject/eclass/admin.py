38ec0b00eb277629af13943ff039c9a30db47e38
HyeonJe Kim <1fprrlxk@naver.com>
2023-11-15 14:39:15 +0900

    local situations

--- back/msgproject/eclass/admin.py
+++ back/msgproject/eclass/admin.py
@@ -3,3 +3,4 @@ from .models import *
 # Register your models here.
 admin.site.register(classlist)
 admin.site.register(UserClasslist)
+admin.site.register(UserProfile)
--- back/msgproject/eclass/permissions.py
+++ back/msgproject/eclass/permissions.py
@@ -1,8 +1,8 @@
-from rest_framework import permissions
+# from rest_framework import permissions
 
 
-class CustomReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.user == request.user
+# class CustomReadOnly(permissions.BasePermission):
+#     def has_object_permission(self, request, view, obj):
+#         if request.method in permissions.SAFE_METHODS:
+#             return True
+#         return obj.user == request.user
