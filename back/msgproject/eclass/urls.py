6d25558f94e175d848cfdc9e1cca686773b5f343
HyeonJe Kim <1fprrlxk@naver.com>
2023-11-16 01:59:47 +0900

    fix: userclass GET/POST

--- back/msgproject/eclass/urls.py
+++ back/msgproject/eclass/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ClassListView
+from .views import ClassListView, UserClassListView
 
 urlpatterns = [
     path('classlist/', ClassListView.as_view(), name='list-classlist'),
+    path('userclasslist/', UserClassListView.as_view(), name='userclasslist'),
 ]
