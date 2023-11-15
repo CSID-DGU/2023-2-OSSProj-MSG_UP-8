cf6f3a7146fa6802e9058f6719ea85373aa00b90
Dahyeonni <2021111552@dgu.ac.kr>
2023-11-15 17:17:26 +0900

    feat: 회원가입 시 강의 선택 get Api연결

--- front/src/pages/Lecturepage.jsx
+++ front/src/pages/Lecturepage.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import * as s from '../style/Lecture.style.js'
 import LectureChoice from "../components/LectureChoice";
 import eclass_logo from '../assets/image/eclass_logo.png';
@@ -6,42 +6,24 @@ import axios from "axios";
 
 function Lecturepage(props) {
 
-    
-    // const get_classpick = async () => {
-    //   try {
-    //     await axios
-    //       .get(
-    //         'http://127.0.0.1:8000/register/classpick',
-    //       )
-    //       .then(res => {
-    //         // console.log(res);
-    //         setProfile(res.data);
-    //         setProfile2(res.data.profile);
-    //         setUser_club_list(res.data.user_club_list);
-    //         setUser_todo_list(res.data.user_todo_list);
-    //         sessionStorage.setItem('userid', res.data.profile.id);
-    //       });
-    //   } catch (err) {
-    //     console.log(err);
-    //   }
-    // };
+    const [lectures, setLectures] = useState([]);
 
-    // useEffect(() => {
-    //   get_profile();
-    // }, []);
-    // 더미
-    const data = [
-        { id: 1, title: "컴퓨터 시스템" },
-        { id: 2, title: "오픈소스 소프트웨어 프로젝트" },
-        { id: 3, title: "융합프로그래밍1" },
-        { id: 4, title: "데이터베이스" },
-        { id: 5, title: "모바일 프로그래밍" },
-        { id: 6, title: "융합프로그래밍2" },
-        { id: 7, title: "오픈소스 소프트웨어 실습" },
-        { id: 8, title: "파이썬프로그래밍" },
-        { id: 9, title: "자료구조 알고리즘" },
-    ]
+    const get_classpick = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/eclass/classlist/');
+        console.log(response.data); 
+        if (Array.isArray(response.data)) { 
+          setLectures(response.data);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
+    useEffect(() => {
+      get_classpick();
+    }, []);
+  
     // const checkList = [...Array(5).fill("체크").map((v, i => v + i))]
 
     const [checkItems, setCheckItems] = useState(new Set());
@@ -71,8 +53,8 @@ function Lecturepage(props) {
                 <s.LectureTitle>수강 과목 입력</s.LectureTitle>
                 <s.CheckBoxList>
                   <s.lectureComponent>
-                    {data.map((item, index) => (
-                        <LectureChoice key={index} id={item.id} title={item.title} checkItemHandler={checkItemHandler} />
+                    {Array.isArray(lectures) && lectures.map((item, index) => (
+                        <LectureChoice key={index} id={item.id} title={item.name} checkItemHandler={checkItemHandler} />
                     ))}
                   </s.lectureComponent>
                   <s.lectureBtn>
