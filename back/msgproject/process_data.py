39f2f761310220dfc1f599eceeb5bfad6d2e85de
HyeonJe Kim <1fprrlxk@naver.com>
2023-11-29 21:52:48 +0900

    fix: 수강과목 모델 및 csv 오류 해결을 위한 롤백

--- back/msgproject/process_data.py
+++ back/msgproject/process_data.py
@@ -20,8 +20,6 @@ conn.close()
 # CSV 파일을 Pandas DataFrame으로 읽기 (헤더 무시)
 df = pd.read_csv('classlist.csv', encoding="euc-kr")
 
-database = "db.sqlite3"
-conn = sqlite3.connect(database)
 
 # 데이터 유형(dtype)을 지정
 dtype = {
@@ -36,6 +34,12 @@ dtype = {
     "place": "CharField",
     "major": "CharField",
 }
+
+# 테이블 열 이름 지정
+df.columns = ['name', 'professor', 'day1', 'day2', 'starttime1', 'endtime1', 'starttime2', 'endtime2', 'place', 'major']
+
+database = "db.sqlite3"
+conn = sqlite3.connect(database)
 
 df.to_sql(name='eclass_classlist', con=conn, if_exists='replace', dtype=dtype, index=True, index_label="id")
 
