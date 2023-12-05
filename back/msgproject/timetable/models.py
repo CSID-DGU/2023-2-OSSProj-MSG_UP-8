3949770ad7e17745fcc69fff9ad3ba12124c431c
HyeonJe Kim <1fprrlxk@naver.com>
2023-12-05 19:40:43 +0900

    style: clean code #32

--- back/msgproject/timetable/models.py
+++ back/msgproject/timetable/models.py
@@ -5,6 +5,3 @@ from eclass.models import UserProfile, UserClasslist
 class UserTimetable(models.Model):
     userprofile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
     userclasses = models.ManyToManyField(UserClasslist)
-
-    # 실제 timetable 구조를 저장하는 방법을 고려해야 합니다.
-    # 예: JSONField를 사용하거나 강의와 시간을 연결하는 별도의 모델 생성
