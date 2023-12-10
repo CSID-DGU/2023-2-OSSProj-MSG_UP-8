352acf5d5563d8c4c8b08061e0d59be4ed30cbf5
HyeonJe Kim <1fprrlxk@naver.com>
2023-12-10 13:36:47 +0900

    fix: process_data.py 누락 내용 복구 #56

--- back/msgproject/process_data.py
+++ back/msgproject/process_data.py
@@ -1,6 +1,22 @@
 import pandas as pd
 import sqlite3
 
+# Read csv file.
+df = pd.read_csv("schedule.csv", encoding="euc-kr")
+
+# Connect to (create) database.
+database = "db.sqlite3"
+conn = sqlite3.connect(database)
+dtype={
+    "out_title": "CharField",
+    "out_startdate": "DateField",
+    "out_enddate": "DateField", 
+}
+
+
+df.to_sql(name='logoutcals_schedule', con=conn, if_exists='replace', dtype=dtype, index=True, index_label="id")
+conn.close()
+
 
 # CSV 파일을 Pandas DataFrame으로 읽기 (헤더가 포함되어 있다고 가정)
 df = pd.read_csv('classlist.csv', encoding="utf-8")
